607f07baa95302dea208001d0676991390fc629b
sndekoning <sylvester.de.koning90@gmail.com>
2016-03-25 11:46:23 +0100

    changed data-file, futher additions to plot

--- ClimateChange-App/app.R
+++ ClimateChange-App/app.R
@@ -1,17 +1,9 @@
 library(shiny)
 library(ggplot2)
-library(dplyr)
-library(lubridate)
 
-climate <- read.csv("./data/GlobalLandTemperaturesByCountry.csv")
+climate <- read.csv("./data/climate.csv")
 cnames <- as.character(unique(climate$Country))
 
-climate$year <- year(climate$dt)
-
-climate<- climate %>% 
-  filter(!is.na(AverageTemperature)) %>%
-  group_by(year, Country) %>% 
-  summarise(YearlyMean = mean(AverageTemperature))
 
 # UI =============
 ui <- fluidPage(
@@ -41,14 +33,33 @@ ui <- fluidPage(
 # Server =========
 server <- function(input, output){
  
+  
   output$plot <- renderPlot({
-    
     x <- switch(input$country.id,
-                )
+                "Ã…land" = climate[ which(climate$Country %in% cnames[1]),],
+                "Albania" = climate[ which(climate$Country %in% cnames[2]),],
+                "Andorra" = climate[ which(climate$Country %in% cnames[3]),],
+                "Austria" = climate[ which(climate$Country %in% cnames[4]),],
+                "Belarus" = climate[ which(climate$Country %in% cnames[5]),],
+                "Belgium" = climate[ which(climate$Country %in% cnames[6]),],
+                "Bosnia And Herzegovina" = climate[ which(climate$Country %in% cnames[7]),],
+                "Bulgaria" = climate[ which(climate$Country %in% cnames[8]),],
+                "Croatia" = climate[ which(climate$Country %in% cnames[9]),],
+                "Czech Republic" = climate[ which(climate$Country %in% cnames[10]),]
+    )
     
-    g <- ggplot(data = climate, aes(x = year, y = YearlyMean))
-    
-  }) 
+    g <- ggplot(data = x, aes(x = year, y = YearlyMean)) +
+      geom_point() +
+      geom_smooth(method = lm)
+
+    return(g)
+
+  })
+  
+  output$summary <- renderPrint({
+    mean(climate$YearlyMean)
+  })
+  
 }
 
 shinyApp(ui = ui, server = server)
--- exploratoryscript.R
+++ exploratoryscript.R
@@ -1,4 +1,8 @@
-data.path <- "./ClimateChange-App/data/GlobalLandTemperaturesByCountry.csv"
+library(ggplot2)
+library(dplyr)
+library(lubridate)
+
+data.path <- "./ClimateChange-App/data/climate.csv"
 climate <- read.csv(data.path)
 
 cnames <- as.character(unique(climate$Country))
@@ -10,3 +14,20 @@ climate<- climate %>%
   summarise(YearlyMean = mean(AverageTemperature))
 
 range(climate$YearlyMean)
+range(climate$year)
+
+
+x <- climate[which(climate$Country %in% "Albania"),]
+
+
+write.csv(climate, file = "./climate.csv")
+
+
+for(i in 1:length(cnames)){
+  cnames[i] = climate[ which(climate$Country %in% cnames[i]),]}
+)
+
+
+g <- ggplot(data = gdata, aes(x = year, y = YearlyMean)) +
+  geom_point() +
+  geom_smooth(method = lm)
